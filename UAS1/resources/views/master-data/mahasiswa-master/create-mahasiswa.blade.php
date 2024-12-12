6d8c5556f93d16a7fdb6cf70658cf666cfdb3518
Muhammad Hafiz Fathurrohman <2210631170137@student.unsika.ac.id>
2024-12-12 23:25:45 +0700

    Final Commit

--- UAS1/resources/views/master-data/mahasiswa-master/create-mahasiswa.blade.php
+++ UAS1/resources/views/master-data/mahasiswa-master/create-mahasiswa.blade.php
@@ -51,7 +51,7 @@
                             </div>
 
                             <button type="submit"
-                                class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Submit</button>
+                                class="inline-flex justify-center px-4 py-2 text-sm font-medium text-black bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Submit</button>
                         </form>
                     </div>
 
